11f9acf98d3a805f21ba5c3637204bbe9f6096f4
Evgenii-A <66975604+Evgenii-A@users.noreply.github.com>
2021-06-13 16:32:47 +0300

    comment line

--- src/main/java/Servlet.java
+++ src/main/java/Servlet.java
@@ -16,7 +16,7 @@ public class Servlet extends HttpServlet {
                 "First Line<br>" +
                 "Second Line<br>" +
                 "Third Line<br>" +
-                "Fourth Line<br>" +
+                //"Fourth Line<br>" +
                 "</h1>");
         writer.println("</html>");
     }
